e38c93868675abc6a294793bb881bb17929466c0
DeSecurity <155940235+DeSecurity@users.noreply.github.com>
2026-04-19 18:59:19 -0700

    Create adenumeration.ps1

---
+++ adenumeration.ps1
@@ -0,0 +1,22 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = $domainObj.PdcRoleOwner.Name
+$DN = ([adsi]'').distinguishedName 
+$LDAP = "LDAP://$PDC/$DN"
+
+$direntry = New-Object System.DirectoryServices.DirectoryEntry($LDAP)
+
+$dirsearcher = New-Object System.DirectoryServices.DirectorySearcher($direntry)
+#$dirsearcher.filter="samAccountType=805306368"
+$dirsearcher.filter="name=jeffadmin"
+$result = $dirsearcher.FindAll()
+
+Foreach($obj in $result)
+{
+    Foreach($prop in $obj.Properties)
+    {
+        #$prop
+        $prop.memberof
+    }
+
+    Write-Host "-------------------------------"
+}
